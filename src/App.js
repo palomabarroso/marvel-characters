ab8b9c8ba623bf48a06b7777db565b99621678d4
Paloma Barroso <palomapbarroso@gmail.com>
2019-07-16 11:44:08 -0300

    Commit inciial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import MarvelCard from './components/MarvelCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const md5 = require('md5');
+const publicKey = '6fd273d42d1cc83dcac6d6d39deff6c8';
+const privateKey = 'e7cd56640d3c01337510c1c682b5a394c967512c';
+const timeStamp = Date.now().toString();
+const hash = md5(timeStamp + privateKey + publicKey);
+const url = (`http://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`);
+
+class App extends Component {
+  state = {
+    heroes: []
+  }
+  async componentDidMount() {
+    const result = await axios.get(url)
+      .then((res) => {
+        return res.data;
+      })
+      .catch((error) => {
+        return error;
+      })
+    const heroes = result.data.results;
+    this.setState({
+      heroes
+    })
+    console.log(heroes)
+  }
+
+  render() {
+    return (
+      <div className="Card">
+        <h1 className="titulo">
+          Marvel Heroes
+        </h1>
+        {this.state.heroes.map((hero) => (
+          <MarvelCard
+            hero={hero}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/MarvelCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+//<img src={hero.thumb[0] ? `${hero.thumbnail[0].path}.${hero.thumbnail[0].extension}` : ''} alt='' title='{hero.title}' />
+const MarvelCard = ({ hero }) => {
+    return (
+        <div key={hero.id} className="card-item">
+            <div className='card-img' >
+                <img src={hero.thumbnail ? `${hero.thumbnail.path}.${hero.thumbnail.extension}` : ''} alt='' title='{hero.title}' />
+            </div>
+            <div className='card-title'>
+                <p>{hero.name}</p>
+            </div>           
+        </div>
+    )
+}
+
+export default MarvelCard;
